a8b1e7e1ba1ecb30754a2d30d9f6b283d9860f87
Eddie <eddie.freitag@gmail.com>
2026-06-09 23:10:07 +0200

    add new models

--- cluedo/instancegenerator.py
+++ cluedo/instancegenerator.py
@@ -39,7 +39,7 @@ ROOMS = [
 
 
 class CluedoInstanceGenerator:
-    def __init__(self, seed=SEED, n_players=2, max_turns=8, n_instances=5):
+    def __init__(self, seed=SEED, n_players=2, max_turns=10, n_instances=10):
         self.seed = seed
         self.n_players = n_players
         self.max_turns = max_turns
